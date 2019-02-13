581730ab1aa052c09ba6ba66f0d863c63d3a5979
derNiklaas <niklask2001@gmail.com>
2019-02-13 18:52:28 +0100

    Initial Release

---
+++ index.js
@@ -0,0 +1,14 @@
+const request = require('request');
+dailyRequest();
+
+function dailyRequest() {
+    request.post("https://www.timolia.de/api/dailypixel", {form: {user: 'derNiklaas'}}, function (err, httpResponse, body) {
+        if (err) {
+            console.log("ERROR: " + err);
+        } else {
+            console.log(body);
+        }
+
+        setTimeout(dailyRequest, 86400000);
+    })
+}
