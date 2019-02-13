4140f5e86104851de617f7a625a70e3d8f80b260
derNiklaas <niklask2001@gmail.com>
2019-02-13 19:02:53 +0100

    Added README

---
+++ README.md
@@ -0,0 +1,13 @@
+#Timolia Pixel Farmer 
+
+This is a small node.js Script for the free daily pixels on the Timolia.de Minecraft Server.
+
+##How to use
+
+1) Download the Repository.
+2) Open the "index.js" File
+3) Change [USERNAME] to your Minecraft Username.
+4) (Optional) Upload all Files to a Server for 24/7 running of the Script
+5) Open a Terminal
+6) Run the command "npm install"
+7) Run the command "node index.js"
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const request = require('request');
 dailyRequest();
 
 function dailyRequest() {
-    request.post("https://www.timolia.de/api/dailypixel", {form: {user: 'derNiklaas'}}, function (err, httpResponse, body) {
+    request.post("https://www.timolia.de/api/dailypixel", {form: {user: '[USERNAME]'}}, function (err, httpResponse, body) {
         if (err) {
             console.log("ERROR: " + err);
         } else {
